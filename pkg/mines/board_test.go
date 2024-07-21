e0a4af012aa265935b287187a66203169493a6fe
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2024-07-21 14:39:18 +0200

    test: more tests for mines adjacency

--- pkg/mines/board_test.go
+++ pkg/mines/board_test.go
@@ -6,7 +6,16 @@ import (
 	"github.com/federico-paolillo/mines/pkg/mines"
 )
 
-func TestAdjacentMinesRecalculateWhenPlacingNewCells(t *testing.T) {
+func TestAdjacentMinesCalculatesProperly(t *testing.T) {
+	/*
+	 * Assume a board like:
+	 * 2 M
+	 * M 2
+	 * where x is a closed empty cell
+	 * 			 1 is a cell with adjacent mines
+	 *       M is a mined cell
+	 */
+
 	board := mines.NewBoard(mines.Size{Width: 2, Height: 2})
 
 	board.PlaceCell(mines.Location{X: 1, Y: 1})
@@ -14,16 +23,95 @@ func TestAdjacentMinesRecalculateWhenPlacingNewCells(t *testing.T) {
 	board.PlaceMine(mines.Location{X: 1, Y: 2})
 	board.PlaceCell(mines.Location{X: 2, Y: 2})
 
-	expectedMines := 2
+	minesExpected := [4]struct {
+		location mines.Location
+		expected int
+	}{
+		{mines.Location{X: 1, Y: 1}, 2},
+		{mines.Location{X: 2, Y: 1}, 1},
+		{mines.Location{X: 1, Y: 2}, 1},
+		{mines.Location{X: 2, Y: 2}, 2},
+	}
 
-	adjacentMinesAt22 := board.AdjacentMines(mines.Location{2, 2})
+	for _, expectation := range minesExpected {
+		adjacentMines := board.AdjacentMines(expectation.location)
 
-	if adjacentMinesAt22 != expectedMines {
-		t.Fatalf(
-			"expected cell at 2,2 to have %d adjacent mines. instead it has %d adjacent mines",
-			expectedMines,
-			adjacentMinesAt22,
-		)
+		if adjacentMines != expectation.expected {
+			t.Errorf(
+				"expected cell at %v to have %d mines. instead it has %d",
+				expectation.location,
+				expectation.expected,
+				adjacentMines,
+			)
+		}
+	}
+}
+
+func TestAdjacentMinesCalculatesProperly2(t *testing.T) {
+	/*
+	 * Assume a board like:
+	 * x x x
+	 * x 1 1
+	 * x 1 M
+	 * x 1 1
+	 * where x is a closed empty cell
+	 * 			 1 is a cell with adjacent mines
+	 *       M is a mined cell
+	 * opening cell a 1,1 should chord and produce a board like:
+	 * # o o
+	 * o 1 1
+	 * o 1 M
+	 * o 1 1
+	 * where o is a an chording empty cell opened
+	 *       # is the cell that was opened
+	 * 			 1 is a cell with adjacent mines
+	 *       M is a mined cell
+	 */
+
+	board := mines.NewBoard(mines.Size{Width: 3, Height: 4})
+
+	board.PlaceCell(mines.Location{X: 1, Y: 1})
+	board.PlaceCell(mines.Location{X: 2, Y: 1})
+	board.PlaceCell(mines.Location{X: 3, Y: 1})
+	board.PlaceCell(mines.Location{X: 1, Y: 2})
+	board.PlaceCell(mines.Location{X: 2, Y: 2})
+	board.PlaceCell(mines.Location{X: 3, Y: 2})
+	board.PlaceCell(mines.Location{X: 1, Y: 3})
+	board.PlaceCell(mines.Location{X: 2, Y: 3})
+	board.PlaceMine(mines.Location{X: 3, Y: 3})
+	board.PlaceCell(mines.Location{X: 1, Y: 4})
+	board.PlaceCell(mines.Location{X: 2, Y: 4})
+	board.PlaceCell(mines.Location{X: 3, Y: 4})
+
+	minesExpected := [12]struct {
+		location mines.Location
+		expected int
+	}{
+		{mines.Location{X: 1, Y: 1}, 0},
+		{mines.Location{X: 2, Y: 1}, 0},
+		{mines.Location{X: 3, Y: 1}, 0},
+		{mines.Location{X: 1, Y: 2}, 0},
+		{mines.Location{X: 2, Y: 2}, 1},
+		{mines.Location{X: 3, Y: 2}, 1},
+		{mines.Location{X: 1, Y: 3}, 0},
+		{mines.Location{X: 2, Y: 3}, 1},
+		{mines.Location{X: 3, Y: 3}, 0},
+		{mines.Location{X: 1, Y: 4}, 0},
+		{mines.Location{X: 2, Y: 4}, 1},
+		{mines.Location{X: 3, Y: 4}, 1},
+	}
+
+	for _, expectation := range minesExpected {
+		adjacentMines := board.AdjacentMines(expectation.location)
+
+		if adjacentMines != expectation.expected {
+			t.Errorf(
+				"expected cell at %v to have %d mines. instead it has %d",
+				expectation.location,
+				expectation.expected,
+				adjacentMines,
+			)
+		}
 	}
 }
 
